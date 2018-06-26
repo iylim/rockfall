32158b359ad1a3dc5e0272e9e77853b464d9308c
Ivy Lim <yunjilim@gmail.com>
2018-06-26 10:36:29 -0700

    board clears

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 /*----- constants -----*/
 var boardSize = 8;
-var rocks = ['images/rock1.png', 'images/rock2.png', 'images/Stone_Icon.png', 'images/rock3.png', 'images/rock4.png', null];
-var diff = {
+var rocks = ['images/rock1.png', 'images/rock2.png', 'images/rock5.png', 'images/rock3.png', 'images/rock4.png', 'images/rock6.png'];
+var points = {
 3: 10,
 4: 20,
 5: 30,
-6: 50
+6: 50,
+7: 70,
+8: 100
+};
+var multiplier = {
+2: 10,
+3: 20,
+4: 30,
+5: 40,
+6: 50,
+7: 60,
+8: 75   
 };
 
 /*----- app's state (variables) -----*/
-var board, firstRockIdx, score;
+var board, firstRockIdx, score, matches;
 var time, timerId;
 
 /*----- cached element references -----*/
@@ -38,22 +49,45 @@ function handleBoardClick(evt) {
     }
     if (!clickCheck(boardIndex)) firstRockIdx = null;
     var matches = getMatches();
-
+    updateScore(matches);
+    clearRocks(matches);
+    collapseBoard(boardIndex);
     render();    
 } 
 
-// function score() {
+function updateScore(matches) {
+    var scoreA = 0;
+    var scoreB = 0;
+    if (multiplier[matches.length]) {
+       scoreA = scoreA + multiplier[matches.length];
+    }
+    for (i = 0; i < matches.length; i++) {
+        scoreB = scoreB + points[matches[i].length];
+    }
+    score = scoreA + scoreB;
+}
     
-//     //update score 
-     
-//     // 2 matches in a row = +10pt bonus
-//     // 3 matches in a row = +20pt bonus 
-//     //4 matches in a row = +30pt bonus
+function clearRocks(matches) {
+    matches.forEach(function(matchArr) {
+        matchArr.forEach(function(boardIndex) {
+            board[boardIndex] = null;
+        });
+    });
+}
 
-//    //clear matched tiles
-//     }
-//     render();
-// }
+function collapseBoard(boardIndex) {
+    for ( i = 63; i >= 0 ; i--) {
+        if (board[boardIndex] === null) {
+            var temp = board[boardIndex];
+            board[boardIndex] = board[boardIndex] - boardSize;
+            temp = board[boardIndex - boardSize];
+            }
+        }
+    }
+
+//function refill(board) {
+//     if (boardIndex === null) getRockIndex();
+// }    
 
 function clickCheck(clickedIdx) {
     if (firstRockIdx === null) {
@@ -76,6 +110,7 @@ function getMatches() {
     // return an array of "match" arrays [10, 11, 12] or [2, 10, 18]
     var matches = getColumnMatches();
     matches.push(...getRowMatches());
+    return matches;
 }
 
 function getColumnMatches() {
@@ -146,6 +181,7 @@ function render() {
     boardImages.forEach(function(img, idx) {
         img.src = rocks[board[idx]];
     });  
+    scoreEl.textContent = score;
 } 
 
 function initBoard() {
@@ -160,9 +196,6 @@ function getRockIndex() {
     return Math.floor(Math.random() * rocks.length);
 }
 
-// play again
-
 initialize();
 
-//scoring
-//go backwards if null fill from the spot above
+//disable click after second until board refilled?
